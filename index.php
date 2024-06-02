2c36dfdd74ea9f48508ba5025d30406f859606f9
root <root@ip-172-31-60-111.ec2.internal>
2024-06-02 17:08:00 +0000

    github auto

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 <body style="background-color:blue">
 
-<h1>My first PHP page with Jenkins Integration and auto push 2 3</h1>
+<h1>My first PHP page with Jenkins Integration and auto push 2 3 4</h1>
 
 <?php
 $output = shell_exec('ifconfig');
