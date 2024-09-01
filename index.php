11c14974d0e5078810b5674b61fab72692b25893
ABHI99-coder <64468233+ABHI99-coder@users.noreply.github.com>
2024-09-02 02:20:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 <body style="background-color:blue">
 
-<h1>My first PHP page with Jenkins Integration and auto push setup from git</h1>
+<h1>Jenkins Integration and auto push setup from git</h1>
 
 <?php
 $output = shell_exec('ifconfig');
